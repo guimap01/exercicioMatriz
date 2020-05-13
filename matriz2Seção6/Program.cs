375f03cde6f00af2f7468a4d588458e2e778f05a
Pedro Guimaraes <pedromonteirogui@gmail.com>
2020-05-13 18:16:33 -0300

    Exercicio matriz

---
+++ matriz2Seção6/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace matriz2Seção6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a quantidade de linha e colunas da matriz");
+            string[] vect = Console.ReadLine().Split(' ');
+
+            int m = int.Parse(vect[0]);
+            int n = int.Parse(vect[1]);
+
+
+            int[,] mat = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                Console.WriteLine("Informe os valores da " + (i + 1) + "º linha da matriz");
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.WriteLine("Informe o numero a ser procurado");
+            int numFind = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] == numFind)
+                    {
+                        Console.WriteLine("Posição: [" + i + "," + j + "]");
+                        if (j != 0)
+                        {
+                            Console.WriteLine("Esquerda: " + mat[i, j - 1]);
+                        }
+                        if (j != n - 1)
+                        {
+                            Console.WriteLine("Direita: " + mat[i, j + 1]);
+                        }
+                        if (i != 0)
+                        {
+                            Console.WriteLine("Acima: " + mat[i - 1, j]);
+                        }
+                        if (i != m - 1)
+                        {
+                            Console.WriteLine("Abaixo: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
